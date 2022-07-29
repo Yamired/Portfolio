d7e16ac3cb9dfebfea0a03d80d3ca737095689f9
Corentin Derimay <71814911+Yamired@users.noreply.github.com>
2022-07-29 10:56:22 +0200

    Project initialization

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+    colorMode: {
+        preference: 'system', // thème par défaut
+        dataValue: 'theme', // propriété data-theme --> <html> tag
+        classSuffix: '',
+    },
+})
